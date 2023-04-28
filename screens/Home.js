07705e968f29e8068c4a9a5668b1b5e5eb933837
Michael Asfaw <72876485+AbyssinianGuy@users.noreply.github.com>
2023-04-27 22:55:25 -0400

    Categories component done

--- screens/Home.js
+++ screens/Home.js
@@ -2,6 +2,7 @@ import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
                 <HeaderTabs />
                 <SearchBar />
             </View>
+            <Categories />
         </SafeAreaView>
     )
 }
