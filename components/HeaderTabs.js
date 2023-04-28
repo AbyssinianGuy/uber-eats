0b88c5e299d2bfb3d8c5438a681034b00b272527
AbyssinianGuy <michaelasfaw12@gmail.com>
2023-04-27 21:35:06 -0400

    dummy headertabs added

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -2,9 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function HeaderTabs() {
-  return (
-    <View>
-      <Text>HeaderTabs</Text>
-    </View>
-  )
+    return (
+        <View>
+            {/* HeaderButton */}
+            <HeaderButton />
+            {/* HeaderButton */}
+        </View>
+    )
 }
+
+const HeaderButton = () => <Text>Delivery</Text>
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import HeaderTabs from '../components/HeaderTabs'
+
 
 export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+    return (
+        <SafeAreaView>
+            <HeaderTabs />
+        </SafeAreaView>
+    )
 }
