f284fc8e2625b8a70fce3111f68d0d88975e683b
Michael Asfaw <72876485+AbyssinianGuy@users.noreply.github.com>
2023-04-29 20:01:30 -0400

    modal view and checkout screen done

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { View, Text, TouchableOpacity, Modal } from 'react-native'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
+import { StyleSheet } from 'react-native-web'
+import OrderItem from './OrderItem'
 
 export default function ViewCart() {
-    const items = useSelector((state) => state.cartReducer.selectedItems.items)
+    const [modalVisible, setModalVisible] = useState(false)
+
+    const { items, restaurantName } = useSelector((state) => state.cartReducer.selectedItems)
     const total = items
         .map((item) => Number(item.price.replace("$", "")))
         .reduce((prev, curr) => prev + curr, 0)
@@ -14,10 +17,96 @@ export default function ViewCart() {
         currency: "USD",
     })
 
-    // console.log(totalUSD, "👍")
+    const tax = (parseFloat(totalUSD.replace('$', '')) * 0.15).toFixed(2)  // 15% tax
+
+    const totalWithTax = (parseFloat(totalUSD.replace('$', '')) + parseFloat(tax)).toFixed(2)  // total + tax
+
+    const styles = StyleSheet.create({
+        modalContainer: {
+            flex: 1,
+            justifyContent: "flex-end",
+            backgroundColor: "rgba(0,0,0,0.7)",
+        },
+        modalCheckoutContainer: {
+            backgroundColor: "white",
+            padding: 16,
+            height: 500,
+            borderWidth: 1,
+        },
+        restaurantName: {
+            textAlign: "center",
+            fontWeight: "600",
+            fontSize: 18,
+            marginTop: 10,
+        },
+        subTotalContainer: {
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: 15,
+        },
+        subTotalText: {
+            textAlign: "left",
+            fontWeight: "600",
+            fontSize: 15,
+            marginBottom: 10,
+        },
+    })
+
+    const checkoutModalContent = () => {
+        return (
+            <>
+                <View style={styles.modalContainer} >
+                    <View style={styles.modalCheckoutContainer}>
+                        <Text style={styles.restaurantName} >Your Order from {restaurantName}</Text>
+                        {items.map((item, index) => (
+                            <OrderItem key={index} item={item} />
+                        ))}
+                        <View style={styles.subTotalContainer}>
+                            <Text style={styles.subTotalText}>Tax (15%)</Text>
+                            <Text>${tax}</Text>
+                        </View>
+                        <View style={styles.subTotalContainer}>
+                            <Text style={styles.subTotalText}>Subtotal</Text>
+                            <Text>${totalWithTax}</Text>
+                        </View>
+                        <View style={{
+                            flexDirection: "row",
+                            justifyContent: "center",
+                        }}>
+                            <TouchableOpacity
+                                style={{
+                                    marginTop: 20,
+                                    backgroundColor: "black",
+                                    flexDirection: "row",
+                                    justifyContent: "center",
+                                    padding: 15,
+                                    borderRadius: 30,
+                                    width: 300,
+                                    position: "relative",
+                                    alignItems: "center",
+                                }}
+                                onPress={() => setModalVisible(false)}
+                            >
+                                <Text style={{ color: "white", fontSize: 20 }}>Checkout </Text>
+                                <Text style={{ color: "white", fontSize: 20 }}>({total ? '$' + totalWithTax : ""})</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </>
+        )
+    }
 
     return (
         <>
+            <Modal
+                animationType='slide'
+                visible={modalVisible}
+                transparent={true}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                {checkoutModalContent()}
+            </Modal>
             {total ? (
                 <View style={{
                     flex: 1,
@@ -45,6 +134,7 @@ export default function ViewCart() {
                                 position: "relative",
                                 // alignItems: "center",
                             }}
+                            onPress={() => setModalVisible(true)}
                         >
                             <Text style={{ color: "white", fontSize: 20, marginRight: 25 }}>View Cart</Text>
                             <Text style={{ color: "white", fontSize: 20 }}>({totalUSD})</Text>
